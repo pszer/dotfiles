a18ce77ede53c6248662a56fa77ba4566db4c2a1
pszerszynski <alienmode1337@gmail.com>
2019-12-01 11:26:23 +0000

    envypn font

--- .i3/i3_dmenu.sh
+++ .i3/i3_dmenu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dmenu -nf '#BBBBBB' -nb '#151614' -sb '#A3DC27' -sf '#000000' -fn 'Unifont' -x 4 -y 4 -w 1912 -h 32 -l 15
+dmenu -nf '#BBBBBB' -nb '#151614' -sb '#A3DC27' -sf '#000000' -fn 'envypn' -x 4 -y 4 -w 1912 -h 32 -l 15
